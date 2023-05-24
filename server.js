fd5625689a5a9d25d2bb4a309cd6a241bb355b5e
raed-msaed <raed.hammouda.msaed@gmail.com>
2023-05-24 14:54:42 +0100

    create service API

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+const express = require('express');
+
+const app = express();
+
+app.post('/add', () => {
+    console.log("add Work");
+})
+
+app.get('/getall', () => {
+    console.log("get All work");
+})
+
+app.put('/update', () => {
+    console.log("update work");
+})
+
+app.delete('/delete', () => {
+    console.log("delete work");
+})
+
+app.listen(3000, () => {
+    console.log('Server run');
+})
